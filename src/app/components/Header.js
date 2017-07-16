80eb5cbc504393f67141d3a3d3b696073461e862
Andrew Eskenazi <drewesk@gmail.com>
2017-07-15 18:54:22 -0600

    First Commit!

---
+++ src/app/components/Header.js
@@ -0,0 +1,15 @@
+import React from 'react';
+
+export class Header extends React.Component {
+  render() {
+    return(
+      <nav className=''>
+        <div>
+          <div>
+            <h3 className="App-header ">This is the Header</h3>
+          </div>
+        </div>
+      </nav>
+    );
+  }
+}
---
+++ src/app/components/Home.js
@@ -0,0 +1,40 @@
+import React from "react";
+
+export class Home extends React.Component {
+  render() {
+    const content = <p>Originally from Seattle I grew up amongst the mass of coffee shops and 'occasional' rainy days. Hobbies consist of playing tons of online chess, cooking, making art, and coding. I hope to develop applications that solve real-world problems and be apart of the constantly booming tech movement</p>;
+
+    return(
+      <body className="wrapper">
+        <main className="articles">
+
+          <p className="App-intro">
+            Welcome to the home-page, {content}
+          </p>
+
+        </main>
+
+        <div className="App-logo">
+          <img src="http://martinkkwan.com/img/techstack/react-icon.png" alt="React" height="30px" width="30px"/>
+          <img src="http://martinkkwan.com/img/techstack/react-icon.png" alt="React" height="30px" width="30px"/>
+          <img src="http://martinkkwan.com/img/techstack/react-icon.png" alt="React" height="30px" width="30px"/>
+          <img src="http://martinkkwan.com/img/techstack/react-icon.png" alt="React" height="30px" width="30px"/>
+          <img src="http://martinkkwan.com/img/techstack/react-icon.png" alt="React" height="30px" width="30px"/>
+          <img src="http://martinkkwan.com/img/techstack/react-icon.png" alt="React" height="30px" width="30px"/>
+          <img src="http://martinkkwan.com/img/techstack/react-icon.png" alt="React" height="30px" width="30px"/>
+          <img src="http://martinkkwan.com/img/techstack/react-icon.png" alt="React" height="30px" width="30px"/>
+          <img src="http://martinkkwan.com/img/techstack/react-icon.png" alt="React" height="30px" width="30px"/>
+          <img src="http://martinkkwan.com/img/techstack/react-icon.png" alt="React" height="30px" width="30px"/>
+          <img src="http://martinkkwan.com/img/techstack/react-icon.png" alt="React" height="30px" width="30px"/>
+          <img src="http://martinkkwan.com/img/techstack/react-icon.png" alt="React" height="30px" width="30px"/>
+          <img src="http://martinkkwan.com/img/techstack/react-icon.png" alt="React" height="30px" width="30px"/>
+          <img src="http://martinkkwan.com/img/techstack/react-icon.png" alt="React" height="30px" width="30px"/>
+          <img src="http://martinkkwan.com/img/techstack/react-icon.png" alt="React" height="30px" width="30px"/>
+          <img src="http://martinkkwan.com/img/techstack/react-icon.png" alt="React" height="30px" width="30px"/>
+          <img src="http://martinkkwan.com/img/techstack/react-icon.png" alt="React" height="30px" width="30px"/>
+        </div>
+
+      </body>
+    );
+  }
+}
